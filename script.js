73bc7c2b25babbf787a6d0e90cc1d80e8dd8c909
Maurien <78821087+MaurienVDL@users.noreply.github.com>
2025-07-08 20:39:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,23 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('footer').innerHTML = data;
     });
 
-  // Toggle buttons
-  document.querySelectorAll('.toggle-btn').forEach(button => {
-    button.addEventListener('click', () => {
-      const group = button.previousElementSibling.querySelectorAll('.collapse');
-
-      group.forEach(item => {
-        item.classList.toggle('show');
-      });
-
-      // Toggle button text
-      if (button.textContent.includes('Show')) {
-        button.textContent = button.textContent.replace('Show', 'Hide');
-      } else {
-        button.textContent = button.textContent.replace('Hide', 'Show');
-      }
-    });
+document.querySelectorAll('.toggle-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const collapseSection = button.previousElementSibling;
+  
+    collapseSection.classList.toggle('show');
+  
+    if (button.textContent.includes('Show')) {
+      button.textContent = button.textContent.replace('Show', 'Hide');
+    } else {
+      button.textContent = button.textContent.replace('Hide', 'Show');
+    }
   });
+});
+
 
   // Header scroll function
   window.addEventListener('scroll', function () {

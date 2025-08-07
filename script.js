53c0ce20a9739835f16e2c71a0b0ebace8f542e9
Maurien <78821087+MaurienVDL@users.noreply.github.com>
2025-08-07 13:11:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // ✅ Knoptekst aanpassen
       if (button.textContent.includes('more')) {
         button.textContent = button.textContent.replace('more', 'less');
-      } 
-      if (button.textContent.includes('less')){
+      } else if (button.textContent.includes('less')){
         button.textContent = button.textContent.replace('less', 'more');
       }
     });

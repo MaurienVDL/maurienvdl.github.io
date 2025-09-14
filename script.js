39b769517409c282f49435cb21dc7d3ec1fb178c
Maurien <78821087+MaurienVDL@users.noreply.github.com>
2025-09-14 12:33:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function adjustAboutOffset() {
     const banner = document.getElementById("site-header");
-    const about = document.querySelector(".about-container");
+    const about = document.querySelector("main");
 
     if (banner && about) {
       const bannerHeight = banner.offsetHeight; // current height in px

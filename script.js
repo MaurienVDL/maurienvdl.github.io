e3b187b778153c3b852e0229a6ac45e402e240b4
Maurien <78821087+MaurienVDL@users.noreply.github.com>
2025-09-14 21:41:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const marginBottom = parseFloat(bannerStyles.marginBottom) || 0;
   
       const bannerHeightWithMargins =
-        banner.offsetHeight + marginTop + marginBottom;
+        banner.offsetHeight;
 
       about.style.setProperty("--banner-offset", bannerHeightWithMargins + "px");
     }
